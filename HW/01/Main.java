722d29f4e442ca2524d41943e9ea3d664fd0d44f
李弘唯 <bbeenn1227@gmail.com>
2020-02-15 23:10:43 +0800

    Create Main.java

---
+++ HW/01/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+	
+	private static Scanner c;
+
+	public static void main(String[] args){
+		
+	//		int b;
+	//int b=new System.in.read();
+		int a=0;
+		while(a==0) {
+		c = new Scanner(System.in);
+		
+		long b= c.nextInt()+ c.nextInt();;
+
+		System.out.println(b);
+		}
+	}
+}
